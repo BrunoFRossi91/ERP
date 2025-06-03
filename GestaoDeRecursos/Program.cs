530f5c46b2d3b242cfa657392cf4cf5825587e67
Bruno Fernandes Rossi <bruno.rossi@cvh.com.br>
2025-06-03 10:50:37 -0300

    cors

--- GestaoDeRecursos/Program.cs
+++ GestaoDeRecursos/Program.cs
@@ -4,63 +4,53 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuração do banco de dados
 builder.Services.AddDbContext<GDRContext>(opts =>
     opts.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
 
-// Adicionando serviços ao container
 builder.Services.AddScoped<EntityCompany>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowOrigin", policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://localhost:3001")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
 });
 
-// Configuração do Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configuração do Kestrel para rodar apenas HTTP na porta 5000
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5000); // Somente HTTP na porta 5000
+    options.ListenAnyIP(5000);
 });
 
 var app = builder.Build();
 
-// Executar Migrations automaticamente
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<GDRContext>();
     dbContext.Database.Migrate();
 }
 
-// Configuração do pipeline HTTP
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "ERP API V1");
-        c.RoutePrefix = string.Empty; // Swagger acessível direto na raiz (ex: http://localhost:5000)
+        c.RoutePrefix = string.Empty; // Swagger direto em http://localhost:5000/
     });
 }
 
-app.UseCors("AllowOrigin");
-
-// Não usamos HTTPS
-// app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 app.MapControllers();
 
-// Rodando apenas HTTP na porta 5000
 app.Run("http://0.0.0.0:5000");
